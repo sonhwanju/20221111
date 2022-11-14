89ba7ea058154b0bf615365d5772305c96d43d40
sonhwanju <hwanjuson@gmail.com>
2022-11-14 14:13:49 +0900

    카메라 스크립트 수정
    카메라의 회전을 만들기 / 장애물 이동 기능 수정

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     [SerializeField]
-    private Transform parentTransform;
+    private Transform cubeTrm;
+    [SerializeField]
+    private Transform cameraTransform;
+
+    private Quaternion cameraRot = Quaternion.identity;
 
     private Vector3 backDir = Vector3.zero;
 
@@ -15,43 +19,86 @@ public class CameraScript : MonoBehaviour
     [SerializeField] private int maxZoomDist = 30;
 
     [SerializeField] private float zoomSpeed = 10f;
-    [SerializeField] private float adjustment = 1f; 
+    [SerializeField] private float rotateSpeed = 10f;
+    [SerializeField] private float adjustment = 1f;
+    [SerializeField] private float rotMinY = -45f;
+    [SerializeField] private float rotMaxY = 45f;
+
+    private float offset = 0f;
 
     private bool canZoomOut;
 
+    private void Start()
+    {
+        cameraRot = transform.localRotation;
+        offset = cameraTransform.localPosition.z;
+    }
+
     void LateUpdate()
     {
-        if (Input.mouseScrollDelta.y == 0) return;
-
-        backDir = transform.position - parentTransform.position;
-
-        if (Input.mouseScrollDelta.y > 0 && backDir.magnitude >= minZoomDist) 
-        {
-            transform.Translate(Vector3.forward * Time.deltaTime * zoomSpeed);
-        }
-        else if (Input.mouseScrollDelta.y < 0 && canZoomOut && backDir.magnitude <= maxZoomDist)
-        {
-            transform.Translate(Vector3.back * Time.deltaTime * zoomSpeed);
-        }
+        Rotate();
+        Zoom();
     } 
 
     void FixedUpdate()
     {
-        backDir = gameObject.transform.position - parentTransform.position;
+        backDir = cameraTransform.position - cubeTrm.position;
 
-        if (Physics.Raycast(parentTransform.position, backDir, out hitInfo, backDir.magnitude))
+        if (Physics.Raycast(cubeTrm.position, backDir, out hitInfo, backDir.magnitude))
         {
-            transform.position = hitInfo.point;
+            cameraTransform.position = hitInfo.point;
+        }
+        else if(Input.mouseScrollDelta.y == 0)
+        {
+            cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, new Vector3(0,0,offset), Time.deltaTime * 3f);
         }
 
-        if (Physics.Raycast(parentTransform.position, backDir, out hitInfo, backDir.magnitude + adjustment))
+        if (Physics.Raycast(cubeTrm.position, backDir, out hitInfo, backDir.magnitude + adjustment))
         {
             canZoomOut = false;
         }
-        else 
+        else
         {
             canZoomOut = true;
         }
 
+    }
+
+    private void Rotate()
+    {
+        if(Input.GetMouseButton(1))
+        {
+            cameraRot.x += -Input.GetAxis("Mouse Y") * rotateSpeed * Time.deltaTime;
+            cameraRot.y += Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime;
+
+            if(cameraRot.x >= rotMaxY)
+            {
+                cameraRot.x = rotMaxY;
+            }
+            else if(cameraRot.x <= rotMinY)
+            {
+                cameraRot.x = rotMinY;
+            }
+
+            transform.localRotation = Quaternion.Euler(cameraRot.x, cameraRot.y, cameraRot.z);
+        }
+    }
+
+    private void Zoom()
+    {
+        if (Input.mouseScrollDelta.y == 0) return;
+
+        backDir = cameraTransform.position - cubeTrm.position;
+
+        if (Input.mouseScrollDelta.y > 0 && backDir.magnitude >= minZoomDist)
+        {
+            cameraTransform.Translate(Vector3.forward * Time.deltaTime * zoomSpeed);
+        }
+        else if (Input.mouseScrollDelta.y < 0 && canZoomOut && backDir.magnitude <= maxZoomDist)
+        {
+            cameraTransform.Translate(Vector3.back * Time.deltaTime * zoomSpeed);
+        }
+
+        offset = cameraTransform.localPosition.z;
     }
 }
